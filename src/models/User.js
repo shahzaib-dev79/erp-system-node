59e7eef396ab57eb3126c7514c2b58d76dd87584
Shumaila Riaz <shumailariaz215@gmail.com>
2026-05-04 14:30:58 +0500

    bug fixed

--- src/models/User.js
+++ src/models/User.js
@@ -2,60 +2,59 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema(
-	{
-		name: {
-			type: String,
-			required: [true, "Name is required"],
-			trim: true,
-			minlength: [2, "Name must be at least 2 characters"],
-		},
-		email: {
-			type: String,
-			required: [true, "Email is required"],
-			unique: true,
-			lowercase: true,
-			trim: true,
-			match: [/^\S+@\S+\.\S+$/, "Please enter a valid email"],
-		},
-		password: {
-			type: String,
-			required: [true, "Password is required"],
-			minlength: [6, "Password must be at least 6 characters"],
-			select: false,
-		},
-		role: {
-			type: String,
-			enum: ["user", "admin", "moderator"],
-			default: "user",
-		},
-		isActive: {
-			type: Boolean,
-			default: true,
-		},
-		refreshToken: {
-			type: String,
-			select: false,
-		},
-	},
-	{
-		timestamps: true,
-	},
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+      minlength: [2, "Name must be at least 2 characters"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [/^\S+@\S+\.\S+$/, "Please enter a valid email"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [6, "Password must be at least 6 characters"],
+      select: false,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin", "moderator"],
+      default: "user",
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    refreshToken: {
+      type: String,
+      select: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
 );
 
-userSchema.pre("save", async function (next) {
-	if (!this.isModified("password")) return next();
-	const salt = await bcrypt.genSalt(12);
-	this.password = await bcrypt.hash(this.password, salt);
-	next();
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  const salt = await bcrypt.genSalt(12);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 userSchema.methods.comparePassword = async function (plainPassword) {
-	return bcrypt.compare(plainPassword, this.password);
+  return bcrypt.compare(plainPassword, this.password);
 };
 
 userSchema.methods.toSafeObject = function () {
-	const { password, refreshToken, __v, ...safe } = this.toObject();
-	return safe;
+  const { password, refreshToken, __v, ...safe } = this.toObject();
+  return safe;
 };
 
 module.exports = mongoose.model("User", userSchema);
