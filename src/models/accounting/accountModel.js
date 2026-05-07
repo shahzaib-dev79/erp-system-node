27282972e61f87da7a2fe8e9bf315330c3728899
Shahzaib Fayyaz <shahzaibdev797@gmail.com>
2026-05-07 06:13:14 +0500

    account crud created

---
+++ src/models/accounting/accountModel.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const accountSchema = new mongoose.Schema({
+	ownerName: {
+		type: String,
+		required: true,
+		minLength: 3,
+	},
+	bankName: String,
+	bankAccountNo: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	type: {
+		type: String,
+		enum: ["bank", "mobile account", "cash"],
+	},
+	balance: Number,
+});
+const accountModel = mongoose.model("account", accountModel);
+module.exports = accountModel;
