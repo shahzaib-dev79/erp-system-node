92c32716a7cb6935f314be09fe6062d2a7111a5e
Shahzaib Fayyaz <shahzaibdev797@gmail.com>
2026-05-20 16:53:37 +0500

    updated routes

--- src/routes/accounting/salesRoutes.js
+++ src/routes/accounting/salesRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 
 const {
-  createSale,
-  getAllSales,
-  getSaleById,
-  updateSale,
-  deleteSale,
+	createSale,
+	getAllSales,
+	getSaleById,
+	updateSale,
+	deleteSale,
 } = require("../../controllers/accounting/saleController");
 const authenticate = require("../../middlewares/authenticate");
 
@@ -13,10 +13,10 @@ const router = express.Router();
 
 router.use(authenticate);
 
-router.post("/", createSale);
-router.get("/", getAllSales);
-router.get("/:id", getSaleById);
-router.put("/:id", updateSale);
-router.delete("/:id", deleteSale);
+router.post("/createSales", createSale);
+router.get("/getAllSales", getAllSales);
+router.get("/getSingleSale/:id", getSaleById);
+router.put("/updateSale/:id", updateSale);
+router.delete("/deleteSale/:id", deleteSale);
 
 module.exports = router;
